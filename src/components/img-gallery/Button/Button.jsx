4acf0f511fb92bab087ab75808e8087376b52c91
arhip14 <kruvdyk1503@gmail.com>
2023-08-26 13:21:08 +0300

    button.jsx edit

--- src/components/img-gallery/Button/Button.jsx
+++ src/components/img-gallery/Button/Button.jsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import buttonStyles from './ButtonStyles';
-
-// const Button = ({ onClick, isVisible }) => (
-//   <button style={buttonStyles.button} onClick={onClick} disabled={!isVisible}>
-//     Load more
-//   </button>
-// );
-
-// Button.propTypes = {
-//   onClick: PropTypes.func.isRequired,
-//   isVisible: PropTypes.bool.isRequired,
-// };
-
-// export default Button;
-
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import buttonStyles from './ButtonStyles';
@@ -40,3 +22,4 @@ Button.propTypes = {
 
 
 export default Button
+
