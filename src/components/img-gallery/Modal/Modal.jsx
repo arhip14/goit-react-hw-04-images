49129369bb2f4c8156d9525a57201d2064384ac8
arhip14 <kruvdyk1503@gmail.com>
2023-08-27 10:16:27 +0300

    modal problem fixed

--- src/components/img-gallery/Modal/Modal.jsx
+++ src/components/img-gallery/Modal/Modal.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Modal = ({ src, alt, onClose }) => {
